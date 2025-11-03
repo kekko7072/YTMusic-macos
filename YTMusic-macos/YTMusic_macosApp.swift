4fe434a3bef005e3b1085a6c2191c0624d6b82ae
Francesco Vezzani <32552589+kekko7072@users.noreply.github.com>
2025-11-03 15:56:20 +0100

    Initial Commit

---
+++ YTMusic-macos/YTMusic_macosApp.swift
@@ -0,0 +1,17 @@
+//
+//  YTMusic_macosApp.swift
+//  YTMusic-macos
+//
+//  Created by Francesco Vezzani on 03/11/25.
+//
+
+import SwiftUI
+
+@main
+struct YTMusic_macosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
